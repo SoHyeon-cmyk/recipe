ec7fe07ee3593ed73051155d843b2de21da65e99
SoHyeon <thguskim12@gmail.com>
2025-01-17 10:00:24 +0900

    0117

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,79 +1,74 @@
-import React, { useContext, useState } from "react";
-import { DataContext } from "../App";
-import { Link } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation, Scrollbar } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import 'swiper/css/scrollbar';
-import List from "../components/List";
-import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import React,{useContext, useState} from 'react';
+import { DataContext } from '../App';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import List from '../conponents/List';
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
+
 
 const Home = () => {
-  const { data } = useContext(DataContext);
-  console.log(data);
+  const {data} = useContext(DataContext);
   const [swiper, setSwiper] = useState(null);
 
   const handlePrev = () => {
-    swiper?.slidePrev();
-  };
+    swiper?.slidePrev()
+  }
   const handleNext = () => {
-    swiper?.slideNext();
-  };
+    swiper?.slideNext()
+  }
   return (
-    <div className="home">
-      {
-        <Swiper
-          className="swiperMain"
+    <div className='home'>
+       <Swiper
           modules={[Autoplay]}
+          className='swiperMain'
           spaceBetween={0}
-          slidesPerView={1}
+          slidesPerView={3}
           autoplay={{
-            delay: 1500,
+            delay: 1000,
             disableOnInteraction: false,
           }}
-          loop
-        >
-          {data.slice(0, 10).map((item) => (
-            <SwiperSlide key={item.id} className="swiperSlide">
-              <Link>
-                <img src={item.ATT_FILE_NO_MK} alt="{item.HASH_TAG}" />
-                <div className="text">
-                  <p className="title">{item.RCP_NM}</p>
-                  <p className="material">{item.RCP_PARTS_DTLS}</p>
-                  <p className="recipe">중량 | {item.INFO_WGT}</p>
-                  <p className="recipe">열량 | {item.INFO_ENG}</p>
-                  <p className="recipe">탄수화물 | {item.INFO_CAR}</p>
-                  <p className="recipe">단백질 | {item.INFO_PRO}</p>
-                  <p className="recipe">지방 | {item.INFO_FAT}</p>
-                  <p className="recipe">나트륨 | {item.INFO_NA}</p>
-                </div>
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      }
-      <div className="title">
-        <h2> 오늘의 추천 레시피</h2>
-      </div>
-      <div className="homeList">
-        <Swiper
-          modules={[Navigation, Autoplay, Scrollbar]}
-          spaceBetween={10}
-          slidesPerView={5}
-          grabCursor={true}
+
           onSwiper={(swiper) => setSwiper(swiper)}
-          scrollbar={{
-            hide: true,
-          }}
         >
-          {data.slice(11, 19).map((item) => (
-            <SwiperSlide key={item.id}>
-              <List data={[item]} />
-            </SwiperSlide>
-          ))}
+          {
+            data.slice(11, 30).map((item) => (
+              <SwiperSlide key={item.id}>
+               <img src={item.ATT_FILE_NO_MK} alt={item.HASH_TAG} />
+              </SwiperSlide>
+            ))
+          }
         </Swiper>
+      <div className="title">
+        <h2> 추천 레시피</h2>
+        <div className="homeList">
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation]}
+         /*  navigation={true} */
+          spaceBetween={50}
+          slidesPerView={3}
+          onSwiper={(swiper) => setSwiper(swiper)}
+        >
+          {
+            data.slice(0, 10).map((item) => (
+              <SwiperSlide key={item.id}>
+                <List data={[item]} />
+              </SwiperSlide>
+            ))
+          }
+         
+        </Swiper>
+        <div className="swiper_btn">
+          <div className="swiperPrevBtn" onClick={handlePrev}><GrPrevious /></div>
+          <div className="swiperNextBtn" onClick={handleNext}><GrNext /></div>
+        </div>
+        </div>
       </div>
+     
+     
     </div>
   );
 };
