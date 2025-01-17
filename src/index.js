9a7995e7cfc807c33537ab13f6968ff157e035c9
SoHyeon <thguskim12@gmail.com>
2025-01-17 09:40:09 +0900

    0117

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouterr} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './scss/init.scss';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouterr basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouterr>
+  </BrowserRouter>
  );
 
 
