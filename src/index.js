010e1dcfb74e9a6ea38d3d826345ee4c6f26d03c
SoHyeon <thguskim12@gmail.com>
2025-01-17 09:37:16 +0900

    0117

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouterr} from 'react-router-dom';
 import './scss/init.scss';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouterr basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>
+  </BrowserRouterr>
  );
 
 
